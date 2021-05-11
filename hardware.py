3aeb08089863fad77070a0b4052e2a257c1c56dd
FelpsOliver <61632746+FelpsOliver@users.noreply.github.com>
2021-05-10 22:53:19 -0300

    Delete hardware.py

--- hardware.py
+++
@@ -1,9 +0,0 @@
-import platform
-
-print('Arquitetura: ', platform.architecture())
-print('Processador: ', platform.processor())
-print('Plataforma: ', platform.platform())
-print('Informações Plataforma: ', platform.machine())
-print('Informações do Sistema: ', platform.system() + ' ' + platform.release())
-
-#Documentação da biblioteca: https://docs.python.org/3/library/platform.html#module-platform
